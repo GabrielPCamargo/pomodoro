b88eba6f5290722b05210d33c4a764eba807dabd
Gabriel Pinos Camargo <gabrielpinos03@gmail.com>
2022-05-18 15:49:07 -0300

    css and main project

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,16 +1,67 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useInterval } from '../hooks/setInterval';
+import { Button } from './button';
+import { Timer } from './timer';
 
 interface PomodoroTimerProps {
-  defaultPomodoroTime: number;
+  pomodoroTime: number;
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 }
 
 export function PomodoroTimer(props: PomodoroTimerProps) {
-  const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
+  const [mainTime, setMainTime] = useState(props.pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+    if (resting) document.body.classList.remove('working');
+  }, [working]);
 
-  return <div>{mainTime}</div>;
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null
+  );
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+    setResting(false);
+    setMainTime(props.pomodoroTime);
+  };
+
+  const configureRest = (long: boolean) => {
+    setTimeCounting(true);
+    setWorking(false);
+    setResting(true);
+
+    if (long) {
+      setMainTime(props.longRestTime);
+    } else {
+      setMainTime(props.shortRestTime);
+    }
+  };
+
+  return (
+    <div className="pomodoro">
+      <h2>You are: working</h2>
+      <Timer mainTime={mainTime} />
+      <div className="controls">
+        <Button text="Work" onClick={configureWork} />
+        <Button text="Rest" onClick={() => configureRest(false)} />
+        <Button
+          className={!working && !resting ? 'hidden' : ''}
+          text={timeCounting ? 'Pause' : 'Resume'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        />
+      </div>
+
+      <div className="details">dlajfdlçajfajfajfakljfakljfa</div>
+    </div>
+  );
 }
